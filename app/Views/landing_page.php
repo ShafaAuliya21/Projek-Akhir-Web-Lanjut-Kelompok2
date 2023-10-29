33ad56e6494d5044a3ffdd3e3cf36d74f87ceee2
Muhammad Al Farrel Elmaretza <alfarrelelmaretza@gmail.com>
2023-10-30 01:12:26 +0700

    feat: landing page

---
+++ app/Views/landing_page.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>Document</title>
+
+    <link rel="stylesheet" href="<?=base_url('assets/css/style.css')?>" />
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@500&display=swap" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+</head>
+<body>
+<header id="header" class="fixed-top">
+    <div class="container d-flex align-items-center">
+        <div class="title">SISPro.</div>
+    </div>
+</header>
+
+    <div class="landing_page">
+        <div class="landing_page_content">
+            <div class="jumbotron">
+                <h1>Selamat Datang di SISPro.</h1>
+                <p>SI Pendaftaran Seminar Proposal.</p>
+                <p>Ilmu Komputer.</p>
+                <a href="#" class="btn btn-primary  btn-custom">Masuk</a>
+                <a href="#" class="btn btn-primary  btn-custom">Daftar</a>
+            </div>
+        </div>
+    </div>
+
+
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+</body>
+</html>
