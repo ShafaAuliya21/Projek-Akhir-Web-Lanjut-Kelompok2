ec88b96c444833596a4fc0fed5f6355c81378eec
Shafa.Auliya21 <shafa.auliya21@students.unila.ac.id>
2023-11-03 03:56:03 +0700

    Mengubah Sign Up

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -4,7 +4,17 @@ namespace App\Controllers;
 
 class Home extends BaseController
 {
-    public function index(): string
+    public function index()
+    {
+        return view('landing_page');
+    }
+
+    public function signin()
+    {
+        return view('sign_in');
+    }
+
+    public function signup()
     {
         return view('sign_up');
     }
