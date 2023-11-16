85f14448847ce531251caf7fb765b83d359be34f
Shafa.Auliya21 <shafa.auliya21@students.unila.ac.id>
2023-11-16 09:45:40 +0700

    Membuat Dashboard Mahasiswa

---
+++ app/Views/dashboard-mahasiswa.php
@@ -0,0 +1,90 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	  <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1">
+        <title>Dashboard
+		</title>
+	    <!-- Bootstrap CSS -->
+        <link rel="stylesheet" href="<?=base_url('assets/css/bootstrap.min.css')?>">
+	    <!----css3---->
+        
+        <link rel="stylesheet" href="<?=base_url('assets/css/dashboard.css')?>">
+        <link rel="stylesheet" href="<?=base_url('assets/css/custom.css')?>">
+		<!-- SLIDER REVOLUTION 4.x CSS SETTINGS -->
+	
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700;900&display=swap" rel="stylesheet">
+	
+	<!--google material icon-->
+        <link href="https://fonts.googleapis.com/css2?family=Material+Icons"
+      rel="stylesheet">
+  </head>
+  <body>
+
+<div class="wrapper">
+
+
+<div class="body-overlay"></div>
+
+
+        <!-- Sidebar  -->
+        <nav id="sidebar">
+            <div class="sidebar-header">
+                <h3><span>SisPro</span></h3>
+            </div>
+            <ul class="list-unstyled components">
+			<li  class="active">
+                    <a href="#" class="dashboard"><i class="material-icons">dashboard</i><span>Dashboard</span></a>
+            </li>
+                
+                <li class="dropdown">
+                    <a href="#pageSubmenu2" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">
+					<i class="material-icons">apps</i><span>Pendaftran</span></a>
+                    <ul class="collapse list-unstyled menu" id="pageSubmenu2">
+                        <li>
+                            <a href="#">Form Berkas</a>
+                        </li>
+                        <li>
+                            <a href="#">Form Pendaftaran</a>
+                        </li>
+                    </ul>
+                </li>
+
+                <li  class="active">
+                    <a href="#" class="dashboard"><i class="material-icons">time</i><span>Jadwal Seminar</span></a>
+                </li>
+				    
+            </ul>
+           
+        </nav>
+		
+     <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+   <script src="<?=base_url('assets/js/jquery-3.3.1.slim.min.js')?>"></script>
+   <script src="<?=base_url('assets/js/popper.min.js')?>"></script>
+   <script src="<?=base_url('assets/js/bootstrap.min.js')?>"></script>
+   <script src="<?=base_url('assets/js/jquery-3.3.1.min.js')?>"></script>
+  
+  
+  <script type="text/javascript">
+  $(document).ready(function () {
+            $('#sidebarCollapse').on('click', function () {
+                $('#sidebar').toggleClass('active');
+				$('#content').toggleClass('active');
+            });
+			
+			 $('.more-button,.body-overlay').on('click', function () {
+                $('#sidebar,.body-overlay').toggleClass('show-nav');
+            });
+			
+        });        
+       
+</script>
+  
+  </body>
+  
+  </html>
